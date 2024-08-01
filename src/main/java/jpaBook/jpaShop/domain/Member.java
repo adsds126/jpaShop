1f610ba7660e2102095ae5929313f0771faf8cf5
gimtaehyeong <adsds126@gmail.com>
2024-08-01 16:54:40 +0900

    settings

--- src/main/java/jpaBook/jpaShop/domain/Member.java
+++ src/main/java/jpaBook/jpaShop/domain/Member.java
@@ -1,4 +1,25 @@
 package jpaBook.jpaShop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+    @Id
+    @Column(name = "MEMBER_ID")
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
 }
--- src/main/java/jpaBook/jpaShop/domain/Order.java
+++ src/main/java/jpaBook/jpaShop/domain/Order.java
@@ -1,4 +1,39 @@
 package jpaBook.jpaShop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+@Getter
+@Setter
 public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
